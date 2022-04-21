2de7f423b8e5ca321d3f0445a3247f0e8a151407
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2022-04-20 23:08:11 -0300

    Starting Training2 - 1044 finished

---
+++ Exercicios/Treinamento2/1044.py
@@ -0,0 +1,18 @@
+from ast import If
+
+A, B = map(int, input().split(" "))
+
+if A > B:
+    if A % B == 0:
+        print('Sao Multiplos')
+    else:
+        print('Nao sao Multiplos')
+
+if A < B:
+    if B % A == 0:
+        print('Sao Multiplos')
+    else:
+        print('Nao sao Multiplos')
+
+if A == B:
+    print('Sao Multiplos')
