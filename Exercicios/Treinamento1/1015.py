9279ea1d5a3e1a1a85b74762fe9327de94e567e2
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2022-04-20 21:32:57 -0300

    Problem 1015 finished

--- Exercicios/Treinamento1/1015.py
+++ Exercicios/Treinamento1/1015.py
@@ -2,5 +2,5 @@ from cmath import sqrt
 
 x1, y1 = map(float, input().split(" "))
 x2, y2 = map(float, input().split(" "))
-distancia = sqrt((x2 - x1)**2 + (y2 - y1)**2)
+distancia = pow((x2 - x1)**2 + (y2 - y1)**2, 1/2)
 print("%0.4f" % distancia)
